a99aca915112104e3115cced5ff0130fff7407c5
WillianSoares93 <cwsm1993@gmail.com>
2025-10-29 20:20:28 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ const urlsToCache = [
   '/',
   'index.html',
   'manifest.json',
-  'https://raw.githubusercontent.com/WillianSoares93/cardapio_samia/refs/heads/main/logo.png',
+  'https://i.imgur.com/bEgTi0O.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
