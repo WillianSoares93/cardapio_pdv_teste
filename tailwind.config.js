d0c6b2c90c614c6531175cc5e776616f37c088db
WillianSoares93 <cwsm1993@gmail.com>
2025-10-30 22:25:18 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Isso diz ao Tailwind para escanear todos os arquivos .html na pasta
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
